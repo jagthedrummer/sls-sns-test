9f5d2b53ff0a0349472a062f9f36fd3ee6db4764
Jeremy Green <jeremy@octolabs.com>
2016-06-09 17:40:38 -0500

    Publish multiple messages at once

--- snsLoader/handler.js
+++ snsLoader/handler.js
@@ -6,15 +6,19 @@ module.exports.handler = function(event, context, cb) {
 
   const sns = new AWS.SNS({params: {TopicArn: process.env.TOPIC_ARN}});
 
-  sns.publish({Message: 'oh hai!'}, function (err, data) {
-    if(err){
-      console.log('Error:', err);
-      return cb({ err });
-    }else{
-      console.log('Message published. Resp:',data);
-      return cb({ data });
-    }
-  });
+  const message = { msg: 'oh hai!', foo: 'bar' };
+
+  for(var i = 0; i < 10; i++){
+    sns.publish({Message: JSON.stringify(message)}, function (err, data) {
+      if(err){
+        console.log('Error:', err);
+        //return cb({ err });
+      }else{
+        console.log('Message published. Resp:',data);
+        //return cb({ data });
+      }
+    });
+  }
 
   return cb(null, {
     message: 'Go Serverless! Your Lambda function executed successfully!'
